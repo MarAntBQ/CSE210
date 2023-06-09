b11b7a93c0aba4895fa7e35e232fb1e3a770c554
Marco Antonio Bustillos Quiroz <repo@code4dev.xyz>
2023-06-08 19:03:09 -0500

    Working fine 1, 2, 3

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What's your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+        bool passed = true;
+
+        if (percent >= 90) {
+            letter = "A";
+        } else if (percent >= 80) {
+            letter = "B";
+        } else if (percent >= 70) {
+            letter = "C";
+        } else if (percent >= 60) {
+            letter = "D";
+            passed = false;
+        } else {
+            letter = "F";
+            passed = false;
+        }
+
+        // Print Result
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (passed) {
+            Console.WriteLine("You passed!");
+        } else {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
